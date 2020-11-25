a1383595842d4fa7665c45d5a8b461596773ac68
timmalstead-cfa <tmalstead@codeforamerica.org>
2020-11-24 18:52:59 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useEffect } from "react"
+import "./App.css"
 
-function App() {
+// const base = ""
+// const key = ""
+
+const App = () => {
+  // useEffect(() => {
+  //   const getRecords = async () => {
+  //     const records = await fetch(
+  //       `https://api.airtable.com/v0/${base}/organization?api_key=${key}`
+  //     )
+  //     const translatedRecords = await records.json()
+  //     console.log(translatedRecords)
+  //   }
+  //   getRecords()
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="app">
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </main>
+  )
 }
 
-export default App;
+export default App
