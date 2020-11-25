c2986840701cbfa417be0dd5c6dc71fe33a47c43
timmalstead-cfa <tmalstead@codeforamerica.org>
2020-11-25 14:06:33 -0800

    sets up simpler login

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useState } from "react"
 import Records from "./Records"
 
 const email = "tmalstead@codeforamerica.org"
-console.log(process.env)
 
 const App = () => {
   const [login, setLogin] = useState(false)
@@ -17,17 +16,6 @@ const App = () => {
       setPassword("")
     }
   }
-
-  // useEffect(() => {
-  //   const getRecords = async () => {
-  //     const records = await fetch(
-  //       `https://api.airtable.com/v0/${base}/organization?api_key=${key}`
-  //     )
-  //     const translatedRecords = await records.json()
-  //     console.log(translatedRecords)
-  //   }
-  //   getRecords()
-  // }, [])
 
   return (
     <main className="app">
